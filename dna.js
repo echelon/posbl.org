cc86906dbe448928222603eeb45e7316393f741f
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-07-19 02:33:24 -0400

    With particle system -- wow

--- dna.js
+++ dna.js
@@ -67,11 +67,39 @@ var Molecule = function()
 			//wireframe: true
 		});
 
-		this.object = new THREE.Mesh(
+		// XXX TEMP
+		function generateSprite() {
+
+			var canvas = document.createElement( 'canvas' );
+			canvas.width = 16;
+			canvas.height = 16;
+
+			var context = canvas.getContext( '2d' );
+			var gradient = context.createRadialGradient( canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2 );
+			gradient.addColorStop( 0, 'rgba(255,255,255,1)' );
+			gradient.addColorStop( 0.2, 'rgba(0,255,255,1)' );
+			gradient.addColorStop( 0.4, 'rgba(0,0,64,1)' );
+			gradient.addColorStop( 1, 'rgba(0,0,0,1)' );
+
+			context.fillStyle = gradient;
+			context.fillRect( 0, 0, canvas.width, canvas.height );
+
+			return canvas;
+
+		}
+
+
+		this.object = new THREE.Particle(
+			new THREE.ParticleBasicMaterial({
+				map: new THREE.Texture(generateSprite()),
+				//map: this.mat,
+				//blending: three.AdditiveBlending;
+		}));
+		/*this.object = new THREE.Mesh(
 			//new THREE.CubeGeometry(rad, rad, rad),
 			new THREE.PlaneGeometry(rad, rad),
 			this.mat
-		);
+		);*/
 
 		if(isCoord) {
 			this.object = new THREE.AxisHelper();
