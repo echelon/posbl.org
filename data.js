67af6a28ab566cce6fc152f56216a8673b7033b0
Brandon Thomas Suit <brandon.suit@gmail.com>
2012-07-22 05:59:42 -0400

    Messy, but ready.

--- data.js
+++ data.js
@@ -17,35 +17,35 @@ var PRESENTATIONS = [
 		date: "April 26, 2012",
 		descr: "Discusses a paper on building a Boolean network representing the T-Cell signaling pathways downstream of the TCR.",
 		img: "http://possibilistic.org/presentation/advimm/img/advimm/matrix.png",
-		url: ""
+		url: "http://possibilistic.org/presentation/advimm/"
 	},
 	{
 		title: "Optimization of GCxGC/MS",
 		date: "Nov, 2011",
 		descr: "Computational tools for optimizing analytical chemistry techniques for exploration of the metabolome.",
 		img: "http://possibilistic.org/presentation/analytical/img/analytical/autosampler.jpg",
-		url: ""
+		url: "http://possibilistic.org/presentation/analytical/"
 	},
 	{
-		title: "Hantavirus Pulmonary Syndrome presentation",
+		title: "Hantavirus Pulmonary Syndrome",
 		date: "Nov 30, 2010",
 		descr: "Presentation on Hantavirus molecular biology and pathology.",
 		img: "http://possibilistic.org/presentation/hps/img/SNV_TEM.jpg",
-		url: ""
+		url: "http://possibilistic.org/presentation/hps/"
 	},
 	{
-		title: "Alzheimer's physiopathy presentation",
+		title: "Alzheimer's physiopathy",
 		date: "April 26, 2010",
 		descr: "My 60-minute presentation on the physiology of Alzheimer's disease with respect to the Amyloid-&beta; and Tau hypothesis. <em>WARNING:</em> Long load time.",
 		img: "http://possibilistic.org/presentation/alzheimers-cell-phys/img/tau-pdb.png",
-		url: ""
+		url: "http://possibilistic.org/presentation/alzheimers-cell-phys/"
 	},
 	{
 		title: "Presentation on C. elegans SNR-5",
 		date: "April 28, 2008",
 		descr: "Slides for a bioinformatics presentation on the Caenorhabditis elegans gene, snr-5. Presents findings on homology, intron/exon prediction, mutants, gene function, etc. (I was a biology novice at this point--I didn't even know what the stages of mitosis were!)",
 		img: "http://possibilistic.org/img/sq_celegans.png",
-		url: ""
+		url: "http://possibilistic.org/presentation/c.elegans-snr5"
 	}
 ]
 
